2eb535f5f7b250d0ffe04ec5a50423b8861ba4c8
MiccWan <michaelwan0906@hotmail.com>
2017-02-12 19:28:17 +0800

    animated

--- app.js
+++ app.js
@@ -7,28 +7,26 @@ var colors = require('colors/safe');
 var port = 7122;
 
 colors.setTheme({
-        setup: ['green','underline'],
-        info: ['grey','underline'],
+        setup: ['magenta','underline'],
+        emphasize: ['green','bold'],
+        info: ['green','underline'],
         error: ['red','underline'],
-        title: ['blue','bold'],
+        title: ['cyan','bold'],
 });
 
 app.use('/',express.static(__dirname+'/static'));
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname+'/pages/index.html',function(){res.end();})
+    res.sendFile(__dirname+'/pages/index.html',function(){
+        getReq("GET","/")
+        res.end();
+    })
 })
 
 app.listen(port, function () {
   console.log( colors.setup('INFOR web listening on port '+port) );
 });
 
-
-
-app.use('/assets',express.static(__dirname+'/assets'));
-app.use('/images',express.static(__dirname+'/images'));
-app.use('/materialize',express.static(__dirname+'/materialize'));
-
-app.get('/',function(req,res){
-	res.sendFile(__dirname+'/pages/index.html',function(){res.end();})
-})
+function getReq(_method,_path){
+    console.log(colors.title("[System] ")+colors.info('Get %s request to %s.'),_method,_path);
+}
